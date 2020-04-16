a59ed13b8f0cd8d7ce247f8acae4ab30cf36d164
gutovesco <guto.vesco@hotmail.com>
2020-04-16 17:48:44 -0300

    user creation working

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import User from '../models/User'
+import {getRepository} from 'typeorm'
+import {hash} from 'bcryptjs'
+
+interface Request{
+  name: string,
+  email: string,
+  password: string
+}
+
+export default class CreateUSerService{
+  public async execute({name, email, password}: Request): Promise<User>{
+    const usersRepository = getRepository(User)
+
+    const checkUserExists = await usersRepository.findOne({
+      where: {email},
+    })
+
+    if(checkUserExists){
+      throw new Error('Email address is already beign used')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword
+    })
+
+    await usersRepository.save(user)
+
+    return user
+  }
+}
